d875600a2681d5942ebef7d395a22b9e0355b5d7
Fernando Abel Ancajima Ancajima <fancajima@lucky.com.pe>
2022-03-04 11:46:38 -0500

    FA-Validar form-login y registro

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -5,7 +5,12 @@ const validarCampos = ( req, res, next ) => {
 
     const errors = validationResult(req);
     if( !errors.isEmpty() ){
-        return res.status(400).json(errors);
+        return res.status(400).json({
+            
+            errors,
+            //Tipo de correo- El correo es obligatorio
+            msg: 'Ingreses un correo válido'
+        });
     }
 
     next();
