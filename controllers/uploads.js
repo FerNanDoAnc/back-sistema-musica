20f6c96d2b9f97d89872feaf1b9a3472c7d2da49
Fernando Abel Ancajima Ancajima <fancajima@lucky.com.pe>
2022-04-11 10:46:19 -0500

    FA-trycatch v.1.0.0

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -12,7 +12,6 @@ const { Usuario, Cancion, Repertorio } = require('../models');
 
 const cargarArchivo = async(req, res = response) => {
 
-
     try {
         
         // txt, md
@@ -22,188 +21,199 @@ const cargarArchivo = async(req, res = response) => {
 
     } catch (msg) {
         res.status(400).json({ msg });
+        console.log("cargarArchivo",error);
     }
 
 }
 
 
 const actualizarImagen = async(req, res = response ) => {
+    try { 
+        const { id, coleccion } = req.params;
 
-    const { id, coleccion } = req.params;
+        let modelo;
 
-    let modelo;
+        switch ( coleccion ) {
+            case 'usuarios':
+                modelo = await Usuario.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un usuario con el id ${ id }`
+                    });
+                }
+            
+            break;
 
-    switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
-            }
+            case 'canciones':
+                modelo = await Cancion.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe una cancion con el id ${ id }`
+                    });
+                }
+            
+            break;
+
+            case 'repertorios':
+                modelo = await Repertorio.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un repertorio con el id ${ id }`
+                    });
+                }
+            
+            break;
         
-        break;
-
-        case 'canciones':
-            modelo = await Cancion.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe una cancion con el id ${ id }`
-                });
-            }
-        
-        break;
-
-        case 'repertorios':
-            modelo = await Repertorio.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un repertorio con el id ${ id }`
-                });
-            }
-        
-        break;
-    
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
-    }
-
-
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
-        // Hay que borrar la imagen del servidor
-        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if ( fs.existsSync( pathImagen ) ) {
-            fs.unlinkSync( pathImagen );
+            default:
+                return res.status(500).json({ msg: 'Se me olvidó validar esto'});
         }
+
+
+        // Limpiar imágenes previas
+        if ( modelo.img ) {
+            // Hay que borrar la imagen del servidor
+            const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+            if ( fs.existsSync( pathImagen ) ) {
+                fs.unlinkSync( pathImagen );
+            }
+        }
+
+
+        const nombre = await subirArchivo( req.files, undefined, coleccion );
+        modelo.img = nombre;
+
+        await modelo.save();
+
+
+        res.json( modelo );
+    } catch (error) {
+        console.log("actualizarImagen",error);
     }
-
-
-    const nombre = await subirArchivo( req.files, undefined, coleccion );
-    modelo.img = nombre;
-
-    await modelo.save();
-
-
-    res.json( modelo );
 
 }
 
 
 const actualizarImagenCloudinary = async(req, res = response ) => {
+    try {
+        const { id, coleccion } = req.params;
 
-    const { id, coleccion } = req.params;
+        let modelo;
 
-    let modelo;
+        switch ( coleccion ) {
+            case 'usuarios':
+                modelo = await Usuario.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un usuario con el id ${ id }`
+                    });
+                }
+            
+            break;
 
-    switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
-            }
+            case 'canciones':
+                modelo = await Cancion.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe una cancion con el id ${ id }`
+                    });
+                }
+            
+            break;
+
+            case 'repertorios':
+                modelo = await Repertorio.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un repertorio con el id ${ id }`
+                    });
+                }
+            
+            break;
         
-        break;
+            default:
+                return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+        }
 
-        case 'canciones':
-            modelo = await Cancion.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe una cancion con el id ${ id }`
-                });
-            }
-        
-        break;
 
-        case 'repertorios':
-            modelo = await Repertorio.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un repertorio con el id ${ id }`
-                });
-            }
+        // Limpiar imágenes previas
+        if ( modelo.img ) {
+            const nombreArr = modelo.img.split('/');
+            const nombre    = nombreArr[ nombreArr.length - 1 ];
+            const [ public_id ] = nombre.split('.');
+            cloudinary.uploader.destroy( public_id );
+        }
+
+
+        const { tempFilePath } = req.files.archivo
+        const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+        modelo.img = secure_url;
+
+        await modelo.save();
+
         
-        break;
-    
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+        res.json( modelo );
+    } catch (error) {
+        console.log("actualizarImagenCloudinary",error);
     }
-
-
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
-        const nombreArr = modelo.img.split('/');
-        const nombre    = nombreArr[ nombreArr.length - 1 ];
-        const [ public_id ] = nombre.split('.');
-        cloudinary.uploader.destroy( public_id );
-    }
-
-
-    const { tempFilePath } = req.files.archivo
-    const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
-    modelo.img = secure_url;
-
-    await modelo.save();
-
-    
-    res.json( modelo );
 
 }
 
 const mostrarImagen = async(req, res = response ) => {
+    try { 
+        const { id, coleccion } = req.params;
 
-    const { id, coleccion } = req.params;
+        let modelo;
 
-    let modelo;
+        switch ( coleccion ) {
+            case 'usuarios':
+                modelo = await Usuario.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un usuario con el id ${ id }`
+                    });
+                }
+            
+            break;
 
-    switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
-            }
+            case 'canciones':
+                modelo = await Cancion.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe una cancion con el id ${ id }`
+                    });
+                }
+            
+            break;
+
+            case 'repertorios':
+                modelo = await Repertorio.findById(id);
+                if ( !modelo ) {
+                    return res.status(400).json({
+                        msg: `No existe un repertorio con el id ${ id }`
+                    });
+                }
+            
+            break;
         
-        break;
-
-        case 'canciones':
-            modelo = await Cancion.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe una cancion con el id ${ id }`
-                });
-            }
-        
-        break;
-
-        case 'repertorios':
-            modelo = await Repertorio.findById(id);
-            if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un repertorio con el id ${ id }`
-                });
-            }
-        
-        break;
-    
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
-    }
-
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
-        // Hay que borrar la imagen del servidor
-        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if ( fs.existsSync( pathImagen ) ) {
-            return res.sendFile( pathImagen )
+            default:
+                return res.status(500).json({ msg: 'Se me olvidó validar esto'});
         }
-    }
 
-    const pathImagen = path.join( __dirname, '../assets/no-image.jpg');
-    res.sendFile( pathImagen );
+        // Limpiar imágenes previas
+        if ( modelo.img ) {
+            // Hay que borrar la imagen del servidor
+            const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+            if ( fs.existsSync( pathImagen ) ) {
+                return res.sendFile( pathImagen )
+            }
+        }
+
+        const pathImagen = path.join( __dirname, '../assets/no-image.jpg');
+        res.sendFile( pathImagen );
+
+    } catch (error) {
+        console.log("mostrarImagen",error);
+    }
 }
 
 
